795bc495fc067ff956268140f0448539ef7dd349
Arpad Vas <avas@corp-ad.credorax.com>
2016-12-15 16:35:59 +0100

    finishing app

--- router.js
+++ router.js
@@ -1,33 +1,54 @@
 var WeatherInfo = require("./weatherInfo.js");
+var renderer = require("./renderer.js");
+var querystring = require("querystring");
 
 function home(request, response) {
 	if (request.url === "/") {
-		response.writeHead(200, {"Content-Type": "text/plain"});
-		response.write("Header\n");
-		response.write("Search\n");
-		response.end("Footer\n");
+		if (request.method.toLowerCase() === "get") {
+			response.writeHead(200, {"Content-Type": "text/html"});
+			renderer.view("header", {}, response);
+			renderer.view("search", {}, response);
+			renderer.view("footer", {}, response);
+			response.end();
+		} else {
+			request.on("data", function(postBody) {
+				var query = querystring.parse(postBody.toString());
+				response.writeHead(303, {"Location": "/" + query.city});
+				response.end();
+			});
+		}
 	}
 }
 
 function user(request, response) {
 	var city = request.url.replace("/", "");
 	if (city.length > 0 ) {
-		response.writeHead(200, {"Content-Type": "text/plain"});
-		response.write("Header\n");
+		response.writeHead(200, {"Content-Type": "text/html"});
+		renderer.view("header", {}, response);
 
 		var weatherInfo = new WeatherInfo(city);
 		weatherInfo.on("end", function(weatherJSON) {
 			var values = {
 				city: weatherJSON.name,
-				temperature: weatherJSON.main.temp
+				temperature: weatherJSON.main.temp,
+				humidity: weatherJSON.main.humidity,
+				wind: weatherJSON.wind.speed,
+				cloudiness: weatherJSON.clouds.all
 			}
-			response.write("The actual wheater in " + values.city + " is " + values.temperature + " degrees.\n");
-			response.end("Footer\n");
+			renderer.view("result", {city: weatherJSON.name,
+				temperature: Math.round(weatherJSON.main.temp),
+				humidity: weatherJSON.main.humidity,
+				wind: Math.round(weatherJSON.wind.speed * 3.6),
+				cloudiness: weatherJSON.clouds.all}, response);
+			renderer.view("footer", {}, response);
+			response.end();
 		});
 
 		weatherInfo.on("error", function(error) {
-			response.write(error.message + "\n");
-			response.end("Footer\n");
+			renderer.view("error", {errorMessage: error.message}, response);
+			renderer.view("search", {}, response);
+			renderer.view("footer", {}, response);
+			response.end();
 		});
 	}
 }
--- weatherInfo.js
+++ weatherInfo.js
@@ -12,13 +12,10 @@ function WeatherInfo(city) {
 
     var request = http.get("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=c55d418452d87000440c7941c24c86d7", function(response) {
         var body = "";
-
         if (response.statusCode !== 200) {
             request.abort();
             profileEmitter.emit("error", new Error("There was an error getting the weather info for " + city + ". (" + http.STATUS_CODES[response.statusCode] + ")"));
         }
-
-        //Read the data
         response.on('data', function (chunk) {
             body += chunk;
             profileEmitter.emit("data", chunk);
@@ -27,7 +24,6 @@ function WeatherInfo(city) {
         response.on('end', function () {
             if(response.statusCode === 200) {
                 try {
-                    //Parse the data
                     var info = JSON.parse(body);
                     profileEmitter.emit("end", info);
                 } catch (error) {
