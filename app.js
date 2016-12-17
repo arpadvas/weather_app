4b606387e39bf933046e85b8e379e707763994d7
Arpad Vas <vas.arpad@gmail.com>
2016-12-17 11:03:29 +0100

    update port

--- app.js
+++ app.js
@@ -5,5 +5,5 @@ var message = "Server running at http://127.0.0.1";
 http.createServer(function(request, response) {
 	router.home(request, response);
 	router.user(request, response);
-}).listen(80, "127.0.0.1");
+}).listen(process.env.PORT || 8080, "127.0.0.1");
 console.log(message.green);
